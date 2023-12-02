0d81871f6a2ff90066c80b77a58f01b298a2e543
ALPINISTLAR <niveam424@gmail.com>
2023-12-02 07:12:17 +0200

    add delete button

--- js/main.js
+++ js/main.js
@@ -2,7 +2,8 @@ document.addEventListener('DOMContentLoaded', function () {
     const inputElement = document.querySelector('.calculator__input');
     const numberButtons = document.querySelectorAll('.key--number');
     const operatorButtons = document.querySelectorAll('.key--operator');
-    const resetButton = document.querySelector('.reset');
+    const resetButton = document.querySelector('.key--reset');
+    const delButton = document.querySelector('.key--del');
 
     function addButtonClickListener(button) {
         button.addEventListener('click', function () {
@@ -13,7 +14,10 @@ document.addEventListener('DOMContentLoaded', function () {
     numberButtons.forEach(addButtonClickListener);
     operatorButtons.forEach(addButtonClickListener);
 
-    resetButton.addEventListener('click', function () {
-        inputElement.value = '';
+
+    delButton.addEventListener('click', function () {
+        if (inputElement.value.length > 0) {
+            inputElement.value = inputElement.value.slice(0, -1);
+        }
     });
 });
