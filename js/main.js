fa033696e224f33b75a918d04b78ec0c36b44d04
ALPINISTLAR <niveam424@gmail.com>
2023-12-02 07:29:52 +0200

    add eval function

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ document.addEventListener('DOMContentLoaded', function () {
     const operatorButtons = document.querySelectorAll('.key--operator');
     const resetButton = document.querySelector('.key--reset');
     const delButton = document.querySelector('.key--del');
+    const equalButton = document.querySelector('.key--equal');
 
     function addButtonClickListener(button) {
         button.addEventListener('click', function () {
@@ -21,6 +22,19 @@ document.addEventListener('DOMContentLoaded', function () {
     delButton.addEventListener('click', function () {
         if (inputElement.value.length > 0) {
             inputElement.value = inputElement.value.slice(0, -1);
+        }
+    });
+
+    equalButton.addEventListener('click', function () {
+        try {
+            // Qiymatni hisoblash
+            const result = eval(inputElement.value);
+
+            // Natijani inputga yozish
+            inputElement.value = result;
+        } catch (error) {
+            // Xato bo'lganda
+            inputElement.value = 'Error';
         }
     });
 });
